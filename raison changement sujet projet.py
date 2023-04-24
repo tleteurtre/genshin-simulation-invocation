96aef65ef4523470a8ea0659123efff6fcb05f00
tom <tleteurtre@gmail.com>
2023-04-24 23:47:11 +0200

    changement de sujet du projet, raison-> dossier correspondant

---
+++ raison changement sujet projet.py
@@ -0,0 +1,3 @@
+#changement projet car la probabilité d'obtenir un personnage ne varie pas selon le nombre d'invocation faite, 
+# j'ai pensé à travailler sur un programme simulant des invocations avec toute sa complexité
+# (avec les différents personnages, les différentes bannières, les différenres armes) cela me motive bien
