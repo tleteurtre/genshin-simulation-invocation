34d425ce17b2b824e514bec529804ea3f3914df0
tom <tleteurtre@gmail.com>
2023-04-24 23:43:32 +0200

    test

---
+++ genshin invocation simulation1.py
@@ -0,0 +1,4 @@
+#infomations brutes :
+# un item (personnage ou armes) 5 étoiles tout les 90 invocations
+#Pour cette bannière ou le personnage mis en avant est Albédo : En prenant en compte qu'on a invoqué un item 5 étoiles, 50% de chance d'obtenir Albédo
+# En prenant en compte que l'item n'est pas Albédo, le prochain personnage 5 étoiles sera forcément Albédo (Garantie)
