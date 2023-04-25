d3824d18ea1faf253098608722638018a05bfbfb
tom <tleteurtre@gmail.com>
2023-04-25 19:47:04 +0200

    Recherche d'information brutes sur le système(1)

--- genshin invocation simulation1.py
+++ genshin invocation simulation1.py
@@ -2,3 +2,12 @@
 # un item (personnage ou armes) 5 étoiles tout les 90 invocations
 #Pour cette bannière ou le personnage mis en avant est Albédo : En prenant en compte qu'on a invoqué un item 5 étoiles, 50% de chance d'obtenir Albédo
 # En prenant en compte que l'item n'est pas Albédo, le prochain personnage 5 étoiles sera forcément Albédo (Garantie)
+#utiliser class pour perso 5 étoiles / 4 étoiles vers la fin ?
+
+
+#proba brutes :
+#taux de drop d'un perso 5 étoiles  : 0,600%, 1,600% lorsque 90 voeux sont fait depuis le dernier personnage 5 étoiles optenu.
+# Dans ces personnages 5 stars, 50% de chance de tomber sur Albédo. Si le personnage optenu n'est pas Albédo, le prochain 5 stars sera forcément Albédo.
+
+#Chercher rajouter les proba des autres personnages, parmi les 50% restants !
+
